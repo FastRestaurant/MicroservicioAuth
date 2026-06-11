739c710d78ee342f35fcef96e70b1392ff438faa
Mathias Cabrera <mathiasdesign1@gmail.com>
2026-06-11 13:07:32 -0300

    Nuevos seeders.

--- Infrastructure/Persistence/Seeders.cs
+++ Infrastructure/Persistence/Seeders.cs
@@ -63,6 +63,145 @@ namespace Infrastructure.Persistence
                     await userManager.AddToRoleAsync(admin, "Admin");
                 }
             }
+            var firstUser = new AppUser
+            {
+                UserName = "Ncamarero",
+                Email = "nicolas@gmail.com",
+                FirstName = "Nicolas",
+                LastName = "Salas",
+                CreatedAt = DateTime.UtcNow
+            };
+            var resultCreateOne = await userManager.CreateAsync(firstUser, "Camarero123");
+            if (resultCreateOne.Succeeded)
+            {
+                await userManager.AddToRoleAsync(firstUser, "Waitress");
+            }
+
+            var secondUser = new AppUser
+            {
+                UserName = "Jcocinero",
+                Email = "jonylapaz@gmail.com",
+                FirstName = "Jonathan",
+                LastName = "La Paz",
+                CreatedAt = DateTime.UtcNow
+            };
+            var resultCreateTwo = await userManager.CreateAsync(secondUser, "Cocinero123");
+            if (resultCreateTwo.Succeeded)
+            {
+                await userManager.AddToRoleAsync(secondUser, "Kitchen");
+            }
+
+            var thirdUser = new AppUser
+            {
+                UserName = "Gcajero",
+                Email = "gaby@gmail.com",
+                FirstName = "Gabriel",
+                LastName = "Navarro",
+                CreatedAt = DateTime.UtcNow
+            };
+            var resultCreateThree = await userManager.CreateAsync(thirdUser, "Cajero123");
+            if (resultCreateThree.Succeeded)
+            {
+                await userManager.AddToRoleAsync(thirdUser, "Cashier");
+            }
+
+            var fourthUser = new AppUser
+            {
+                UserName = "Fcajero",
+                Email = "fede@gmail.com",
+                FirstName = "Federico",
+                LastName = "Fradera",
+                CreatedAt = DateTime.UtcNow
+            };
+            var resultCreateFour = await userManager.CreateAsync(fourthUser, "Cajero123");
+            if (resultCreateFour.Succeeded)
+            {
+                await userManager.AddToRoleAsync(fourthUser, "Cashier");
+            }
+
+            var fifthUser = new AppUser
+            {
+                UserName = "Tcamarero",
+                Email = "tobias@gmail.com",
+                FirstName = "Tobías",
+                LastName = "Masksymowicz",
+                CreatedAt = DateTime.UtcNow
+            };
+            var resultCreateFive = await userManager.CreateAsync(fifthUser, "Camarero123");
+            if (resultCreateFive.Succeeded)
+            {
+                await userManager.AddToRoleAsync(fifthUser, "Waitress");
+            }
+
+            var sixthUser = new AppUser
+            {
+                UserName = "Gcocinero",
+                Email = "germanV@gmail.com",
+                FirstName = "Germán",
+                LastName = "Vallejos",
+                CreatedAt = DateTime.UtcNow
+            };
+            var resultCreateSix = await userManager.CreateAsync(sixthUser, "Cocinero123");
+            if (resultCreateSix.Succeeded)
+            {
+                await userManager.AddToRoleAsync(sixthUser, "Kitchen");
+            }
+
+            var seventhUser = new AppUser
+            {
+                UserName = "Ncajero",
+                Email = "nahuc@gmail.com",
+                FirstName = "Nahuel",
+                LastName = "Coronel",
+                CreatedAt = DateTime.UtcNow
+            };
+            var resultCreateSeven = await userManager.CreateAsync(seventhUser, "Cajero123");
+            if (resultCreateSeven.Succeeded)
+            {
+                await userManager.AddToRoleAsync(seventhUser, "Cashier");
+            }
+
+            var eighthUser = new AppUser
+            {
+                UserName = "Jcamarero",
+                Email = "rodriguesJ@gmail.com",
+                FirstName = "Juan",
+                LastName = "Rodrigues",
+                CreatedAt = DateTime.UtcNow
+            };
+            var resultCreateEight = await userManager.CreateAsync(eighthUser, "Camarero123");
+            if (resultCreateEight.Succeeded)
+            {
+                await userManager.AddToRoleAsync(eighthUser, "Waitress");
+            }
+
+            var ninethUser = new AppUser
+            {
+                UserName = "Lcocinero",
+                Email = "leonelp@gmail.com",
+                FirstName = "Leonel",
+                LastName = "Paco",
+                CreatedAt = DateTime.UtcNow
+            };
+            var resultCreateNine = await userManager.CreateAsync(ninethUser, "Cocinero123");
+            if (resultCreateNine.Succeeded)
+            {
+                await userManager.AddToRoleAsync(ninethUser, "Kitchen");
+            }
+
+            var newUser = new AppUser
+            {
+                UserName = "Fcajero",
+                Email = "gomezf@gmail.com",
+                FirstName = "Franco",
+                LastName = "Gomez",
+                CreatedAt = DateTime.UtcNow
+            };
+            var newResultCreate = await userManager.CreateAsync(newUser, "Cajero123");
+            if (newResultCreate.Succeeded)
+            {
+                await userManager.AddToRoleAsync(newUser, "Cashier");
+            }
         }
     }
 }
